b9aec50f3c339b69e2a4b7c4e9f84396104af8c0
Harshit <harshit214saxena@gmail.com>
2024-04-06 19:14:33 +0000

    basic file added

---
+++ basics/basic1.js
@@ -0,0 +1,14 @@
+const accountId = 1324
+let accountEmail = "harsh"
+var accountPass = "12345"
+accountCity = "Sitapur"
+let accountState;
+// accountId = 2 
+
+accountEmail = "hchs"
+accountPass = "22233"
+accountCity = "lucknow"
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPass, accountCity, accountState])
